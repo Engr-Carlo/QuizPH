a74c10c627129e92c12eac090925a26558c7a05d
Engr-Carlo <carlobisa2000@gmail.com>
2026-07-04 10:49:42 +0800

    fixing websocket

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,9 +31,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="manifest" href="/manifest.json" />
-      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
